7034f32474f9a4f72751916ad9707eb25e209f83
Edward Do <edward.do@leaplegalsoftware.com>
2023-01-09 20:29:40 +1100

    section 2 code

--- max/understanding-ts/app.js
+++ max/understanding-ts/app.js
@@ -1,1 +1,12 @@
-console.log("is this thing on...?");
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favouriteActivities;
+favouriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- max/understanding-ts/app.ts
+++ max/understanding-ts/app.ts
@@ -1,1 +1,14 @@
-console.log("is this thing on...?");
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+	userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+	throw { message: message, errorCode: code };
+}
+
+generateError('An error occurred!', 500);
